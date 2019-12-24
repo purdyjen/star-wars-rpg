07fe6dfe597ca613b76dae9472f043100e246809
purdyjen <purdyjen@me.com>
2019-12-24 09:38:12 -0600

    pushing before help session

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -24,32 +24,32 @@ $('#qui-gon').on("click", function() {
     $('.obi-wan').addClass('hidden');
     $('.luke').addClass('hidden');
     $('.rey').addClass('hidden');
+    $('this').addClass('selected-attacker');
     $('#jedi-opponents').removeClass('hidden');
-    
 });
 
 $('#obi-wan').on("click", function() {
     $('.qui-gon').addClass('hidden');
     $('.luke').addClass('hidden');
     $('.rey').addClass('hidden');
+    $('this').addClass('selected-attacker');
     $('#jedi-opponents').removeClass('hidden');
-    
 });
 
 $('#luke').on("click", function() {
     $('.obi-wan').addClass('hidden');
     $('.qui-gon').addClass('hidden');
     $('.rey').addClass('hidden');
+    $('this').addClass('selected-attacker');
     $('#jedi-opponents').removeClass('hidden');
-   
 });
 
 $('#rey').on("click", function() {
     $('.obi-wan').addClass('hidden');
     $('.luke').addClass('hidden');
     $('.qui-gon').addClass('hidden');
+    $('this').addClass('selected-attacker');
     $('#jedi-opponents').removeClass('hidden');
-   
 });
 
 //sith
@@ -57,32 +57,32 @@ $('#sidious').on("click", function() {
     $('.maul').addClass('hidden');
     $('.vader').addClass('hidden');
     $('.kylo').addClass('hidden');
+    $('this').addClass('selected-attacker');
     $('#sith-opponents').removeClass('hidden');
-   
 });
 
 $('#maul').on("click", function() {
     $('.sidious').addClass('hidden');
     $('.vader').addClass('hidden');
     $('.kylo').addClass('hidden');
+    $('this').addClass('selected-attacker');
     $('#sith-opponents').removeClass('hidden');
-    
 });
 
 $('#vader').on("click", function() {
     $('.maul').addClass('hidden');
     $('.sidious').addClass('hidden');
     $('.kylo').addClass('hidden');
+    $('this').addClass('selected-attacker');
     $('#sith-opponents').removeClass('hidden');
-    
 });
 
 $('#kylo').on("click", function() {
     $('.maul').addClass('hidden');
     $('.sidious').addClass('hidden');
     $('.vader').addClass('hidden');
+    $('this').addClass('selected-attacker');
     $('#sith-opponents').removeClass('hidden');
-    
 });
 
 // *~*~*~*~*~*~*~ Choose an opponent *~*~*~*~*~*~*~
@@ -92,41 +92,52 @@ $('#sidious2').on("click", function() {
     $('.maul2').addClass('hidden');
     $('.vader2').addClass('hidden');
     $('.kylo2').addClass('hidden');
-    $('jedi-opponents').addClass('hidden');
-    $('jedi-characters').addClass('hidden');
+    $('this').addClass('selected-defender');
     $('.arena').removeClass('hidden');
+    $('.selected-attacker').removeClass('hidden');
+    $('.selected-defender').removeClass('hidden');
     $('#attacker-sith').addClass('hidden');
     $('#defender-jedi').addClass('hidden');
-    
+    $('#selection-complete').addClass('hidden');
 });
 
 $('#maul2').on("click", function() {
     $('.sidious2').addClass('hidden');
     $('.vader2').addClass('hidden');
     $('.kylo2').addClass('hidden');
-    $('jedi-opponents').addClass('hidden');
-    $('jedi-characters').addClass('hidden');
+    $('this').addClass('selected-defender');
     $('.arena').removeClass('hidden');
+    $('.selected-attacker').removeClass('hidden');
+    $('.selected-defender').removeClass('hidden');
     $('#attacker-sith').addClass('hidden');
     $('#defender-jedi').addClass('hidden');
+    $('#selection-complete').addClass('hidden');
 });
 
 $('#vader2').on("click", function() {
     $('.maul2').addClass('hidden');
     $('.sidious2').addClass('hidden');
     $('.kylo2').addClass('hidden');
+    $('this').addClass('selected-defender');
+    $('.arena').removeClass('hidden');
+    $('.selected-attacker').removeClass('hidden');
+    $('.selected-defender').removeClass('hidden');
     $('#attacker-sith').addClass('hidden');
     $('#defender-jedi').addClass('hidden');
-    
+    $('#selection-complete').addClass('hidden');
 });
 
 $('#kylo2').on("click", function() {
     $('.maul2').addClass('hidden');
     $('.sidious2').addClass('hidden');
     $('.vader2').addClass('hidden');
+    $('this').addClass('selected-defender');
+    $('.arena').removeClass('hidden');
+    $('.selected-attacker').removeClass('hidden');
+    $('.selected-defender').removeClass('hidden');
     $('#attacker-sith').addClass('hidden');
     $('#defender-jedi').addClass('hidden');
-    
+    $('#selection-complete').addClass('hidden');
 });
 
 //sith opponents
@@ -134,36 +145,52 @@ $('#qui-gon2').on("click", function() {
     $('.obi-wan2').addClass('hidden');
     $('.luke2').addClass('hidden');
     $('.rey2').addClass('hidden');
+    $('this').addClass('selected-defender');
+    $('.arena').removeClass('hidden');
+    $('.selected-attacker').removeClass('hidden');
+    $('.selected-defender').removeClass('hidden');
     $('#defender-sith').addClass('hidden');
     $('#attacker-jedi').addClass('hidden');
-   
+    $('#selection-complete').addClass('hidden');
 });
 
 $('#obi-wan2').on("click", function() {
     $('.qui-gon2').addClass('hidden');
     $('.luke2').addClass('hidden');
     $('.rey2').addClass('hidden');
+    $('this').addClass('selected-defender');
+    $('.arena').removeClass('hidden');
+    $('.selected-attacker').removeClass('hidden');
+    $('.selected-defender').removeClass('hidden');
     $('#defender-sith').addClass('hidden');
     $('#attacker-jedi').addClass('hidden');
-    
+    $('#selection-complete').addClass('hidden');
 });
 
 $('#luke2').on("click", function() {
     $('.obi-wan2').addClass('hidden');
     $('.qui-gon2').addClass('hidden');
     $('.rey2').addClass('hidden');
+    $('this').addClass('selected-defender');
+    $('.arena').removeClass('hidden');
+    $('.selected-attacker').removeClass('hidden');
+    $('.selected-defender').removeClass('hidden');
     $('#defender-sith').addClass('hidden');
     $('#attacker-jedi').addClass('hidden');
-    
+    $('#selection-complete').addClass('hidden');
 });
 
 $('#rey2').on("click", function() {
     $('.obi-wan2').addClass('hidden');
     $('.luke2').addClass('hidden');
     $('.qui-gon2').addClass('hidden');
+    $('this').addClass('selected-defender');
+    $('.arena').removeClass('hidden');
+    $('.selected-attacker').removeClass('hidden');
+    $('.selected-defender').removeClass('hidden');
     $('#defender-sith').addClass('hidden');
     $('#attacker-jedi').addClass('hidden');
-    
+    $('#selection-complete').addClass('hidden');
 });
 
 
